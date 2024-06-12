0bfff96234ee26ca98c5a3afde910dbeff154bfc
Tr0ya7 <devluizhenrique@proton.me>
2024-06-12 02:18:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exrc_for { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro :");
+
+            int value = int.Parse(Console.ReadLine()); //valor deve começar como uma string para evitar erros lógicos no for
+
+            for (int i = 1; i <= value; i++) //começar como i = 1 para evitar erros
+            {   
+                if (value % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
